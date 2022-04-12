5a36f578fbf7dcd4dd8cdc29d329f696a17b9b91
Cristopher Rivas <cristopher_rivas@outlook.es>
2022-04-12 10:24:58 -0500

    12 de abril

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GUBS'`, () => {
+  it(`should have as title 'gubs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GUBS');
+    expect(app.title).toEqual('gubs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GUBS app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gubs app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpperMenuComponent } from './components/upper-menu/upper-menu.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ManifestoComponent } from './components/manifesto/manifesto.component';
+import { ChellengeComponent } from './components/chellenge/chellenge.component';
+import { ApproachComponent } from './components/approach/approach.component';
+import { LinksComponent } from './components/links/links.component';
+import { PrincipesComponent } from './components/principes/principes.component';
+import { MainComponent } from './pages/main/main.component';
+import { CultureComponent } from './pages/culture/culture.component';
+import { BrandsComponent } from './pages/brands/brands.component';
+import { RoadmapsComponent } from './pages/roadmaps/roadmaps.component';
+import { ConspiraciesComponent } from './pages/conspiracies/conspiracies.component';
+
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+import { NotesComponent } from './components/notes/notes.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpperMenuComponent,
+    MainMenuComponent,
+    SliderComponent,
+    ManifestoComponent,
+    ChellengeComponent,
+    ApproachComponent,
+    LinksComponent,
+    PrincipesComponent,
+    MainComponent,
+    CultureComponent,
+    BrandsComponent,
+    RoadmapsComponent,
+    ConspiraciesComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    NoopAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
